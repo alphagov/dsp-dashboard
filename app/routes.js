a35f14d2c514f6111d88e64d9be46b00f09ec2b0
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2025-01-06 16:39:03 +0000

    Add data source for Notify performance data
    Use that data source for automatic updates.

--- app/routes.js
+++ app/routes.js
@@ -3,26 +3,31 @@ const router = govukPrototypeKit.requests.setupRouter()
 const cache = require('memory-cache')
 const date = new Date()
 
-const CACHE_KEY = 'performanceData'
-const CACHE_TTL = 60000 // Time to live in milliseconds (e.g., 60 seconds)
+const CACHE_TTL = 120000 // Time to live in milliseconds (e.g., 2 minutes)
 
 router.get('/', async (req, res) => {
   res.locals.month = date.toLocaleString('default', { month: 'long' })
   res.locals.year = date.getFullYear()
 
-  const payData = await fetchData('https://raw.githubusercontent.com/alphagov/pay-product-page/refs/heads/main/data/performance.json')
+  const payData = await fetchData('https://raw.githubusercontent.com/alphagov/pay-product-page/refs/heads/main/data/performance.json', 'payDataCache')
 
-  let totalPaymentAmount = payData['totalPaymentAmount'].replace('billion', '')
+  let totalPaymentAmount = payData['totalPaymentAmount'].replace('billion', 'B')
   res.locals.totalPaymentAmount = totalPaymentAmount
-  res.locals.numberOfPayments = payData['numberOfPayments'].replace('million', '')
+  res.locals.numberOfPayments = payData['numberOfPayments'].replace('million', 'M')
   res.locals.payNumberOfServices = payData['numberOfServices']
   res.locals.payNumberOfOrganisations = payData['numberOfOrganisations']
+
+  const notifyData = await fetchData('https://www.notifications.service.gov.uk/features/performance.json', 'notifyDataCache')
+  console.log(notifyData)
+  res.locals.notifyTotalMsgs = (notifyData['total_notifications'] / 1e9).toFixed(1) + "B"
+  res.locals.notifyLive = notifyData['count_of_live_services_and_organisations']['services']
+  res.locals.notifyOrgs = notifyData['count_of_live_services_and_organisations']['organisations']
 
   res.render('index')
 })
 
-const fetchData = async (url) => {
-  const cachedData = cache.get(CACHE_KEY)
+const fetchData = async (url, cacheKey) => {
+  const cachedData = cache.get(cacheKey)
   if (cachedData) {
     console.log('Returning cached data')
     return cachedData
@@ -34,7 +39,7 @@ const fetchData = async (url) => {
       throw new Error('Network response was not ok ' + response.statusText)
     }
     const data = await response.json()
-    cache.put(CACHE_KEY, data, CACHE_TTL)
+    cache.put(cacheKey, data, CACHE_TTL)
     return data
   } catch (error) {
     console.error('There has been a problem with your fetch operation:', error)
