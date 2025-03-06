5189c8c2f878130849fe335cc89968b7cc6cec52
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2025-03-06 14:40:27 +0000

    Update forms numbers to be from a data source
    This makes the dashboard fully automatic.

--- app/routes.js
+++ app/routes.js
@@ -12,7 +12,7 @@ router.get('/', async (req, res) => {
   const rawPayData = await fetchData('https://api.github.com/repos/alphagov/pay-product-page/contents/data/performance.json?ref=main', 'payDataCache')
   const payData = JSON.parse(Buffer.from(rawPayData['content'], "base64").toString("utf-8"))
 
-  let totalPaymentAmount = payData['totalPaymentAmount'].replace(' billion', '&hairsp;B')
+  const totalPaymentAmount = payData['totalPaymentAmount'].replace(' billion', '&hairsp;B')
   res.locals.totalPaymentAmount = totalPaymentAmount
   res.locals.numberOfPayments = payData['numberOfPayments'].replace(' million', '&hairsp;M')
   res.locals.payNumberOfServices = payData['numberOfServices']
@@ -23,6 +23,11 @@ router.get('/', async (req, res) => {
   res.locals.notifyTotalMsgs = (notifyData['total_notifications'] / 1e9).toFixed(1) + "&hairsp;B"
   res.locals.notifyLive = notifyData['count_of_live_services_and_organisations']['services']
   res.locals.notifyOrgs = notifyData['count_of_live_services_and_organisations']['organisations']
+
+  const formData = await fetchData('https://www.forms.service.gov.uk/performance.json')
+  res.locals.formSubmissions = formData['submissions']
+  res.locals.formsPublished = formData['published']
+  res.locals.formOrganisations = formData['organisations']
 
   res.render('index')
 })
